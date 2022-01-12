1dfaedf46501ca49931583ff9d4059443fa3f31f
Gabriel Diac <gabriel.diac@maxcode.net>
2022-01-12 18:49:30 +0200

    added script for downloading icons from figma

---
+++ flag-icons/figma.config.js
@@ -0,0 +1,28 @@
+const svgo = require('@figma-export/transform-svg-with-svgo')
+
+require("dotenv").config();
+
+const fileId = process.env.FILE_ID;
+const outputters = [
+  require("@figma-export/output-components-as-svg")({ output: "./icons" }),
+];
+
+/** @type {import('svgo').PluginConfig[]} */
+const optimizerPlugins = [
+    { removeDimensions: true, name: "removeDimensions" }, //switch from dimenstions to viewbox
+  ];
+
+/** @type {import('@figma-export/types').FigmaExportRC} */
+module.exports = {
+  commands: [
+    [
+      "components",
+      {
+        fileId,
+        onlyFromPages: ["Flags"],
+        transformers: [svgo({ multipass: true, plugins: optimizerPlugins })],
+        outputters,
+      },
+    ]
+  ],
+};
