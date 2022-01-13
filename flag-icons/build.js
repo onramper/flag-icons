f639e4ac66fa217999980683060ac684f99e3dc8
Gabriel Diac <gabriel.diac@maxcode.net>
2022-01-13 12:25:02 +0200

    added component that renders icons based on country code

--- flag-icons/build.js
+++ flag-icons/build.js
@@ -1,14 +1,18 @@
+// (c) https://github.com/mikunpham/react-icon-boilerplate/build.js
+// This code is licensed under MIT license (see LICENSE.txt for details)
+
 const fs = require('fs/promises');
 const rimraf = require('rimraf');
 const svgr = require('@svgr/core').transform;
 const camelcase = require('camelcase');
 const babel = require('@babel/core');
 const { minify } = require('terser');
+const generateCountryIconComponent = require("./country");
 
 const outputPath = './dist';
-const inputPath = './icons/Flags';
+const inputPaths = ['./icons/Flags', './icons/other'];
 
-async function transformSVGtoJSX(file, componentName, format) {
+async function transformSVGtoJSX(inputPath, file, componentName, format) {
   const content = await fs.readFile(`${inputPath}/${file}`, 'utf-8');
   const svgReactContent = await svgr(
     content,
@@ -44,16 +48,26 @@ async function transformSVGtoJSX(file, componentName, format) {
 function indexFileContent(files, format, includeExtension = true) {
   let content = '';
   const extension = includeExtension ? '.js' : '';
-  files.map((fileName) => {
-    const componentName = `${camelcase(fileName.replace(/.svg/, ''), {
-      pascalCase: true,
-    })}Icon`;
+
+  const appendToContent = (componentName) => {
     const directoryString = `'./${componentName}${extension}'`;
     content +=
       format === 'esm'
         ? `export { default as ${componentName} } from ${directoryString};\n`
         : `module.exports.${componentName} = require(${directoryString});\n`;
+  }
+
+  files.map((fileName) => {
+    const componentName = `${camelcase(fileName.replace(/.svg/, ''), {
+      pascalCase: true,
+    })}Icon`;
+    appendToContent(componentName);
   });
+
+  ["CountryIcon"].forEach(componentName => {
+    appendToContent(componentName);
+  });
+
   return content;
 }
 
@@ -61,30 +75,39 @@ async function buildIcons(format = 'esm') {
   const outDir = outputPath;
   await fs.mkdir(outDir, { recursive: true });
 
-  const files = await fs.readdir(inputPath, 'utf-8');
-  await Promise.all(
-    files.flatMap(async (fileName) => {
-      const componentName = `${camelcase(fileName.replace(/.svg/, ''), {
-        pascalCase: true,
-      })}Icon`;
-      const content = await transformSVGtoJSX(fileName, componentName, format);
-      const types = `import * as React from 'react';\ndeclare function ${componentName}(props: React.SVGProps<SVGSVGElement>): JSX.Element;\nexport default ${componentName};\n`;
+  let allFiles = [];
 
-      // console.log(`- Creating file: ${componentName}.js`);
-      await fs.writeFile(`${outDir}/${componentName}.js`, content, 'utf-8');
-      await fs.writeFile(`${outDir}/${componentName}.d.ts`, types, 'utf-8');
-    })
-  );
+  for(let  i = 0; i < inputPaths.length; i++) {
+    const inputPath = inputPaths[i];
+    const files = await fs.readdir(inputPath, 'utf-8');
+    await Promise.all(
+      files.flatMap(async (fileName) => {
+        const componentName = `${camelcase(fileName.replace(/.svg/, ''), {
+          pascalCase: true,
+        })}Icon`;
+        const content = await transformSVGtoJSX(inputPath, fileName, componentName, format);
+        const types = `import * as React from 'react';\ndeclare function ${componentName}(props: React.SVGProps<SVGSVGElement>): JSX.Element;\nexport default ${componentName};\n`;
+  
+        // console.log(`- Creating file: ${componentName}.js`);
+        await fs.writeFile(`${outDir}/${componentName}.js`, content, 'utf-8');
+        await fs.writeFile(`${outDir}/${componentName}.d.ts`, types, 'utf-8');
+      })
+    );
+
+    allFiles = [...allFiles, ...files];
+  }
+
+  await generateCountryIconComponent(outDir);
 
   console.log('- Creating file: index.js');
   await fs.writeFile(
     `${outDir}/index.js`,
-    indexFileContent(files, format),
+    indexFileContent(allFiles, format),
     'utf-8'
   );
   await fs.writeFile(
     `${outDir}/index.d.ts`,
-    indexFileContent(files, 'esm', false),
+    indexFileContent(allFiles, 'esm', false),
     'utf-8'
   );
 }
